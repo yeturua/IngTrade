f2e90c9f2ac47f2f6072e8015699813091696817
unknown <advitha99@gmail.com>
2019-04-04 22:07:31 +0530

    Initial Project structure

---
+++ src/main/java/com/ing/trade/IngTradeApplication.java
@@ -0,0 +1,13 @@
+package com.ing.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngTradeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ing/trade/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ing.trade;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IngTradeApplication.class);
+	}
+
+}
